9c3ce46a80d3daacf8d5a4fe4b1cf9021d89bf50
Junaid Shaikh <44426749+Junshaikh@users.noreply.github.com>
2025-05-31 00:56:44 +0400

    Update setup.py

--- generate-test-cases/setup.py
+++ generate-test-cases/setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="generate-tests",
-    version="0.1",
+    name="generate-test-cases",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "google-generativeai",
-        "requests"
+        "requests",
+        "python-dotenv"
     ],
     entry_points={
         "console_scripts": [
-            "generate-tests=generate_tests.cli:main"
-        ],
+            "generate-tests = generate_tests.cli:main"
+        ]
     },
 )
